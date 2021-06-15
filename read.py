79d66b21abd4a9c2d59c3058d8e699612aaf4c7e
Sky Man King <skymankingtam@gmail.com>
2021-06-16 03:22:07 +0700

    hoan thanh time, hello, time

--- read.py
+++ read.py
@@ -1,5 +1,6 @@
 import pyttsx3
 def talk(say):
+    print(say)
     e = pyttsx3.init()
     e.say(say)
     e.runAndWait()
--- test.py
+++ test.py
@@ -1,10 +1,26 @@
-import read
-import listen
-import hello
+import read as r
+import listen as l
+import hello_time
 import time
 
 name = 'Tam'
+sayf = ['say','bye']
 
-read.talk(hello.get_time("1"))
+def main():
+    while True:
+        say = input()
+        if 'hello' in say:
+            r.talk(hello_time.hello(name))
+        elif 'bye' in say:
+            r.talk('Good bye')
+            break
+        elif 'now' in say:
+            r.talk(hello_time.get_time(say))
 
 
+        else:
+            r.talk(sayf[1])
+
+
+
+main()
